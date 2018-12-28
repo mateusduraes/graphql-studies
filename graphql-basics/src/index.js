b3a1d5112e1c7c967a982bea11e1d688e8467c4f
mduraes1994 <mduraes1994@gmail.com>
2018-12-27 22:55:22 -0200

    Finish imports / exports

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,5 +1,6 @@
-import myCurrentLocation, { message, name } from "./myModule";
+import myCurrentLocation, { message, name, getGreeting } from "./myModule";
 
 console.log(`Message: ${message}`);
 console.log(`Name: ${name}`);
 console.log(`My current location: ${myCurrentLocation}`);
+console.log(getGreeting('Mateus'));
--- graphql-basics/src/myModule.js
+++ graphql-basics/src/myModule.js
@@ -1,7 +1,9 @@
 // Named export - Has a name. Have as many as needed;
 const message = 'Some message from myModule.js';
 const name = 'Mateus';
+const getGreeting = (name) => `Welcome to the course ${name}`;
+
 // Default export - Has no name. You can only have one.
 const location = 'Brazil';
 
-export { message, name, location as default };
+export { message, name, getGreeting, location as default };
