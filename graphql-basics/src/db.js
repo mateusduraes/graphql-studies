6734e26044933e160476bfb4770a218349baac68
mduraes1994 <mduraes1994@gmail.com>
2018-12-30 23:07:28 -0200

    Update files to the last session

--- graphql-basics/src/db.js
+++ graphql-basics/src/db.js
@@ -1,74 +1,64 @@
-// Scalar types ->  String, Boolea, Int, Float, ID
-const users = [
-  {
+const users = [{
     id: '1',
-    name: 'Mateus',
-    email: 'mateus@example.com',
-    age: 24
-  },
-  {
+    name: 'Andrew',
+    email: 'andrew@example.com',
+    age: 27
+}, {
     id: '2',
-    name: 'Sara',
-    email: 'sara@example.com',    
-  },
-  {
+    name: 'Sarah',
+    email: 'sarah@example.com'
+}, {
     id: '3',
     name: 'Mike',
-    email: 'mike@example.com',    
-    age: 31,    
-  },
-];
+    email: 'mike@example.com'
+}]
 
-const posts = [
-  {
-    id: '1',
-    title: "GraphQL",
-    body: 'Hello GraphQL',
-    published: true,
-    author: '1',
-  },
-  {
-    id: '2',
-    title: "JavaScript",
-    body: 'Javascript is good',
+const posts = [{
+    id: '10',
+    title: 'GraphQL 101',
+    body: 'This is how to use GraphQL...',
     published: true,
     author: '1'
-  },
-  {
-    id: '3',
-    title: "Ionic",
-    body: 'Ionic is good',
+}, {
+    id: '11',
+    title: 'GraphQL 201',
+    body: 'This is an advanced GraphQL post...',
+    published: false,
+    author: '1'
+}, {
+    id: '12',
+    title: 'Programming Music',
+    body: '',
     published: true,
     author: '2'
-  }
-];
+}]
 
-const comments = [
-  {
-    id: '1',
-    text: 'Hello 1',
-    author: '1',
-    post: '1',
-  },
-  {
-    id: '2',
-    text: 'Hello 2',
-    author: '2',
-    post: '2',
-  },
-  {
-    id: '3',
-    text: 'Hello 3',
-    author: '2',
-    post: '2',
-  },
-  {
-    id: '4',
-    text: 'Hello 4',
+const comments = [{
+    id: '102',
+    text: 'This worked well for me. Thanks!',
     author: '3',
-    post: '1',
-  },
-]
+    post: '10'
+}, {
+    id: '103',
+    text: 'Glad you enjoyed it.',
+    author: '1',
+    post: '10'
+}, {
+    id: '104',
+    text: 'This did no work.',
+    author: '2',
+    post: '11'
+}, {
+    id: '105',
+    text: 'Nevermind. I got it to work.',
+    author: '1',
+    post: '12'
+}]
 
-const db = { users, posts, comments };
-export { db as default };
+const db = {
+    users,
+    posts,
+    comments
+}
+
+export { db as default }
