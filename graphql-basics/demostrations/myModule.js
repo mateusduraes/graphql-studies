caac7ace9cb06ac63522dfbde55ec2f85b371e42
Mateus <mduraes1994@gmail.com>
2019-01-03 15:47:59 -0200

    Reestructure project

--- graphql-basics/demostrations/myModule.js
+++
@@ -1,9 +0,0 @@
-// Named export - Has a name. Have as many as needed;
-const message = 'Some message from myModule.js';
-const name = 'Mateus';
-const getGreeting = (name) => `Welcome to the course ${name}`;
-
-// Default export - Has no name. You can only have one.
-const location = 'Brazil';
-
-export { message, name, getGreeting, location as default };
